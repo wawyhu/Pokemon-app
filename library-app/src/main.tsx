2f0991fe36ea212ce58f8bb3196dc653697b805f
Wahyu Nur Cahyono <wahyunurcahyonooo@gmail.com>
2023-11-27 21:35:55 +0700

    first commit

---
+++ library-app/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <p className='text-3xl text-center text-red-600'>Hello World!</p>
+  </React.StrictMode>,
+)
